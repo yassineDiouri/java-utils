2ada3786f333c8d99f12bbd43583eb18c9186e90
Yassine DIOURI <yassinediouri92@gmail.com>
2016-10-09 23:01:56 +0200

    [DEV] MemDB Utils Version 0.0.11

--- src/main/java/org/binx/utils/database/memdb/manager/TableManager.java
+++ src/main/java/org/binx/utils/database/memdb/manager/TableManager.java
@@ -111,6 +111,49 @@ public abstract class TableManager {
 	public static Table getTable(String tableName) {
 		return SchemaManager.getTable(tableName);
 	}
+	
+	/**
+	 * Return List of Tables on specified database..schema
+	 * 
+	 * @param databaseName
+	 * @param schemaName
+	 * @return
+	 * Null if database..schema not exist
+	 */
+	public static List<Table> getAllTables(String databaseName, String schemaName) {
+		return SchemaManager.getAllTables(databaseName, schemaName);
+	}
+	/**
+	 * Return List of Tables on specified database..default(schema)
+	 * 
+	 * @param databaseName
+	 * @return
+	 * Null if database not exist
+	 */
+	public static List<Table> getAllTables(String databaseName) {
+		return SchemaManager.getAllTables(databaseName);
+	}
+
+	/**
+	 * Return List of Tables on specified default(database)..schema
+	 * 
+	 * @param schemaName
+	 * @return
+	 * Null if no default(database)..schema
+	 */
+	public static List<Table> getAllTablesDefaultDB(String schemaName) {
+		return SchemaManager.getAllTablesDefaultDB(schemaName);
+	}
+
+	/**
+	 * Return List of Tables on specified default(database)..default(schema)
+	 * 
+	 * @return
+	 * Null if no default(database)
+	 */
+	public static List<Table> getAllTables() {
+		return SchemaManager.getAllTables();
+	}
 
 	/**
 	 * Delete Table from specified database..schema
@@ -219,7 +262,290 @@ public abstract class TableManager {
 	public static Boolean exists(String tableName) {
 		return SchemaManager.existsTable(tableName);
 	}
+	
+	/**
+	 * add column into specified table on database..schema 
+	 * 
+	 * @param databaseName
+	 * @param schemaName
+	 * @param tableName
+	 * @param column
+	 * @return
+	 * True if added<br/>
+	 * False if not<br/>
+	 * null if table, schema or database not exists
+	 */
+	public static Boolean addColumn(String databaseName, String schemaName, String tableName, Column<?> column) {
+		Table tab = getTable(databaseName, schemaName, tableName);
+		if(tab != null) {
+			return tab.getColumns().add(column);
+		}
+		return null;
+	}
 
+	/**
+	 * add column into specified table on database..default(schema) 
+	 * 
+	 * @param databaseName
+	 * @param tableName
+	 * @param column
+	 * @return
+	 * True if added<br/>
+	 * False if not<br/>
+	 * null if table or database not exists
+	 */
+	public static Boolean addColumn(String databaseName, String tableName, Column<?> column) {
+		Table tab = getTable(databaseName, tableName);
+		if(tab != null) {
+			return tab.getColumns().add(column);
+		}
+		return null;
+	}
+
+	/**
+	 * add column into specified table on default(database)..schema 
+	 * 
+	 * @param schemaName
+	 * @param tableName
+	 * @param column
+	 * @return
+	 * True if added<br/>
+	 * False if not<br/>
+	 * null if table, schema or default database not exists
+	 */
+	public static Boolean addColumnDefaultDB(String schemaName, String tableName, Column<?> column) {
+		Table tab = getTableDefaultDB(schemaName, tableName);
+		if(tab != null) {
+			return tab.getColumns().add(column);
+		}
+		return null;
+	}
+
+	/**
+	 * add column into specified table on default(database)..default(schema) 
+	 * 
+	 * @param tableName
+	 * @param column
+	 * @return
+	 * True if added<br/>
+	 * False if not<br/>
+	 * null if table or default database not exists
+	 */
+	public static Boolean addColumn(String tableName, Column<?> column) {
+		Table tab = getTable(tableName);
+		if(tab != null) {
+			return tab.getColumns().add(column);
+		}
+		return null;
+	}
+	
+	/**
+	 * Get Column from specified table on database..schema
+	 * 
+	 * @param databaseName
+	 * @param schemaName
+	 * @param tableName
+	 * @param columnName
+	 * @return
+	 * Null if table not exist or database..schema not exist
+	 */
+	public static Column<?> getColumn(String databaseName, String schemaName, String tableName, String columnName) {
+		if(existsColumn(databaseName, schemaName, tableName, columnName)) {
+			for(Column<?> col : getAllColumns(databaseName, schemaName, tableName))
+				if(col.getName().equals(columnName))
+					return col;
+		}
+		return null;
+	}
+
+	/**
+	 * Get Column from specified table on database..default(schema)
+	 * 
+	 * @param databaseName
+	 * @param tableName
+	 * @param columnName
+	 * @return
+	 * Null if table or database not exist
+	 */
+	public static Column<?> getColumn(String databaseName, String tableName, String columnName) {
+		if(existsColumn(databaseName, tableName, columnName)) {
+			for(Column<?> col : getAllColumns(databaseName, tableName))
+				if(col.getName().equals(columnName))
+					return col;
+		}
+		return null;
+	}
+	
+	/**
+	 * Get Column from specified table on default(database)..schema
+	 * 
+	 * @param schemaName
+	 * @param tableName
+	 * @param columnName
+	 * @return
+	 * Null if table or schema not exist, or no default database
+	 */
+	public static Column<?> getColumnDefaultDB(String schemaName, String tableName, String columnName) {
+		if(existsColumnDefaultDB(schemaName, tableName, columnName)) {
+			for(Column<?> col : getAllColumnsDefaultDB(schemaName, tableName))
+				if(col.getName().equals(columnName))
+					return col;
+		}
+		return null;
+	}
+
+	/**
+	 * Get Column from specified table on default(database)..default(schema)
+	 * 
+	 * @param schemaName
+	 * @param tableName
+	 * @param columnName
+	 * @return
+	 * Null if table not exist, or no default database
+	 */
+	public static Column<?> getColumn(String tableName, String columnName) {
+		if(existsColumn(tableName, columnName)) {
+			for(Column<?> col : getAllColumns(tableName))
+				if(col.getName().equals(columnName))
+					return col;
+		}
+		return null;
+	}
+	
+	/**
+	 * Return List of Columns from specified table on database..schema
+	 * 
+	 * @param databaseName
+	 * @param schemaName
+	 * @param tableName
+	 * @return
+	 * Null if table or database..schema not exist
+	 */
+	public static List<Column<?>> getAllColumns(String databaseName, String schemaName, String tableName) {
+		if(exists(databaseName, schemaName, tableName)) {
+			return getTable(databaseName, schemaName, tableName).getColumns();
+		}
+		return null;
+	}
+
+	/**
+	 * Return List of Columns from specified table on database..default(schema)
+	 * 
+	 * @param databaseName
+	 * @param tableName
+	 * @return
+	 * Null if table or database not exist
+	 */
+	public static List<Column<?>> getAllColumns(String databaseName, String tableName) {
+		if(exists(databaseName, tableName)) {
+			return getTable(databaseName, tableName).getColumns();
+		}
+		return null;
+	}
+	
+	/**
+	 * Return List of Columns from specified table on default(database)..schema
+	 * 
+	 * @param schemaName
+	 * @param tableName
+	 * @return
+	 * Null if table or schema not exist or no default database
+	 */
+	public static List<Column<?>> getAllColumnsDefaultDB(String schemaName, String tableName) {
+		if(existsDefaultDB(schemaName, tableName)) {
+			return getTableDefaultDB(schemaName, tableName).getColumns();
+		}
+		return null;
+	}
+
+	
+	/**
+	 * Return List of Columns from specified table on default(database)..default(schema)
+	 * 
+	 * @param schemaName
+	 * @param tableName
+	 * @return
+	 * Null if table not exist or no default database
+	 */
+	public static List<Column<?>> getAllColumns(String tableName) {
+		if(exists(tableName)) {
+			return getTable(tableName).getColumns();
+		}
+		return null;
+	}
+	
+	/**
+	 * Verify if column exists on specified table in database..schema
+	 * 
+	 * @param databaseName
+	 * @param schemaName
+	 * @param tableName
+	 * @param columnName
+	 * @return
+	 * True if exist<br/>
+	 * else False
+	 */
+	public static Boolean existsColumn(String databaseName, String schemaName, String tableName, String columnName) {
+		List<String> names = getAllNamesColumns(databaseName, schemaName, tableName);
+		if(names != null) {
+			if(names.contains(columnName)) return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Verify if column exists on specified table in database..default(schema)
+	 * 
+	 * @param databaseName
+	 * @param tableName
+	 * @param columnName
+	 * @return
+	 * True if exist<br/>
+	 * else False
+	 */
+	public static Boolean existsColumn(String databaseName, String tableName, String columnName) {
+		List<String> names = getAllNamesColumns(databaseName, tableName);
+		if(names != null) {
+			if(names.contains(columnName)) return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Verify if column exists on specified table in default(database)..schema
+	 * 
+	 * @param schemaName
+	 * @param tableName
+	 * @param columnName
+	 * @return
+	 * True if exist<br/>
+	 * else False
+	 */
+	public static Boolean existsColumnDefaultDB(String schemaName, String tableName, String columnName) {
+		List<String> names = getAllNamesColumnsDefaultDB(schemaName, tableName);
+		if(names != null) {
+			if(names.contains(columnName)) return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Verify if column exists on specified table in default(database)..default(schema)
+	 * 
+	 * @param tableName
+	 * @param columnName
+	 * @return
+	 * True if exist<br/>
+	 * else False
+	 */
+	public static Boolean existsColumn(String tableName, String columnName) {
+		List<String> names = getAllNamesColumns(tableName);
+		if(names != null) {
+			if(names.contains(columnName)) return true;
+		}
+		return false;
+	}
+	
 	/**
 	 * List all names of existing columns on specified table from database..schema
 	 * 
